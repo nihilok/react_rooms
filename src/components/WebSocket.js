537112ebb7825255eab172d5a70c23e00d5cac21
nihilok <mjfullstack@gmail.com>
2021-05-28 11:42:32 +0100

    working rooms with leave and reconnect

--- src/components/WebSocket.js
+++ src/components/WebSocket.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, useCallback} from "react";
 import socketIOClient from "socket.io-client";
 import ConnectedIndicator from "./ConnectedIndicator";
 
@@ -6,119 +6,161 @@ const ENDPOINT = "192.168.1.95:8000";
 
 const WebSocket = () => {
 
-    const socket = useRef(null)
-    const [roomName, setRoomName] = useState('')
-    const [userName, setUserName] = useState('')
-    const [connected, setConnected] = useState(false)
-    const [inRoom, setInRoom] = useState(false)
-    const [roomData, setRoomData] = useState({})
-    const initialInputClass = "helper-text flex"
-    const initialInputClasses = {user: initialInputClass, room: initialInputClass}
-    const [inputClasses, setInputClasses] = useState(initialInputClasses)
-
-
-    const handleChange = (e) => {
-        switch (e.target.name) {
-            default:
-                return
-            case "userName":
-                setUserName(e.target.value);
-                if (e.target.value) {
-                    setInputClasses(() => ({
-                        ...inputClasses,
-                        user: initialInputClass + ' user'
-                    }))
-                } else {
-                    setInputClasses(() => ({
-                        ...inputClasses,
-                        user: initialInputClass
-                    }))
-                }
-                break;
-            case "roomName":
-                setRoomName(e.target.value);
-                if (e.target.value) {
-                    setInputClasses(() => ({
-                        ...inputClasses,
-                        room: initialInputClass + ' room'
-                    }))
-                } else {
-                    setInputClasses(() => ({
-                        ...inputClasses,
-                        room: initialInputClass
-                    }))
-                }
-                break;
+        const socket = useRef(null)
+        const [roomName, setRoomName] = useState('')
+        const [userName, setUserName] = useState('')
+        const [connected, setConnected] = useState(false)
+        const [inRoom, setInRoom] = useState(false)
+        const initialRoomData = {
+            room_name: '',
+            players: [],
+            host: ''
         }
-    }
+        const [roomData, setRoomData] = useState(initialRoomData)
+        const initialInputClass = "helper-text flex"
+        const initialInputClasses = {user: initialInputClass, room: initialInputClass}
+        const [inputClasses, setInputClasses] = useState(initialInputClasses)
+        const [token, setToken] = useState(null)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        joinCreateRoom();
-    }
 
-    const joinCreateRoom = () => {
-        socket.current.emit("new_player", roomName, userName, (data) => {
-            setRoomData(data);
-            setInRoom(true);
-        });
-    }
-
-    const checkRoomName = (roomName) => {
-        if (roomName === roomData.room_name) {
-            return true
-        }
-    }
-
-    useEffect(() => {
-        socket.current = socketIOClient(ENDPOINT);
-        socket.current.on("connect", () => {
-            setConnected(true);
-        });
-        socket.current.on("disconnect", () => {
-            setConnected(false);
-            setInRoom(false);
-        });
-        socket.current.on("update room", (data) => {
-            if (checkRoomName(data.room_name)) {
-            setRoomData(roomData => ({
-                ...roomData,
-                players: data.players
-            }));
+        const handleChange = (e) => {
+            switch (e.target.name) {
+                default:
+                    return
+                case "userName":
+                    setUserName(e.target.value);
+                    if (e.target.value) {
+                        setInputClasses(() => ({
+                            ...inputClasses,
+                            user: initialInputClass + ' user'
+                        }))
+                    } else {
+                        setInputClasses(() => ({
+                            ...inputClasses,
+                            user: initialInputClass
+                        }))
+                    }
+                    break;
+                case "roomName":
+                    setRoomName(e.target.value);
+                    if (e.target.value) {
+                        setInputClasses(() => ({
+                            ...inputClasses,
+                            room: initialInputClass + ' room'
+                        }))
+                    } else {
+                        setInputClasses(() => ({
+                            ...inputClasses,
+                            room: initialInputClass
+                        }))
+                    }
+                    break;
             }
-        });
+        }
 
-    }, []);
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            joinCreateRoom();
+        }
+
+        const joinCreateRoom = () => {
+            socket.current.emit("new_player", roomName, userName, localStorage.getItem('token'), (data) => {
+                if (!data.message) {
+                    setRoomData(data);
+                    setInRoom(true);
+                } else {
+                    alert(data.message);
+                }
+            });
+        }
+
+        const leaveRoom = () => {
+            console.log(token)
+            socket.current.emit('leave_room', roomName, token, () => {
+                setInRoom(false);
+                setRoomData(initialRoomData)
+            })
+
+        }
+
+        const checkRoomName = useCallback((roomName) => {
+            if (roomName === roomData.room_name) {
+                return true
+            }
+        }, [roomData.room_name])
+
+        const handleToken = useCallback(() => {
+            socket.current.emit('get_token', (data) => {
+                setToken(data);
+                if (data !== localStorage.getItem('token')) {
+                    console.log(token)
+                    console.log(data)
+                    localStorage.setItem('token', data);
+                }
+                setConnected(true)
+            })
+        }, [token])
+
+        useEffect(() => {
+            socket.current = socketIOClient(ENDPOINT);
+            socket.current.on("connect", () => {
+                handleToken();
+            });
+            socket.current.on("connect_error", (e) => {
+                console.log(e.message);
+            })
+            socket.current.on("disconnect", () => {
+                setConnected(false);
+                setInRoom(false);
+            });
+            socket.current.on("update room", (data) => {
+                if (checkRoomName(data.room_name)) {
+                    setRoomData(roomData => ({
+                        ...roomData,
+                        room_name: data.room_name,
+                        players: data.players
+                    }));
+                }
+            });
+            // return () => {
+            //     if (inRoom) {
+            //         console.log('leaving room');
+            //         socket.emit('client_disconnect')
+            //     }
+            // }
+
+        }, [checkRoomName, handleToken]);
 
 
-    return (
-        <div className="login-screen">
-            <ConnectedIndicator connected={connected}/>
-            {!inRoom ? (
-                <form onSubmit={handleSubmit} className={"join-room-form flex-col flex-center"}>
-                    <div className={inputClasses.room}><input type="text" value={roomName} onChange={handleChange}
-                                                              placeholder="Room Name" name="roomName"/></div>
-                    <div className={inputClasses.user}><input type="text" value={userName} onChange={handleChange}
-                                                              placeholder="Nickname" name="userName"/></div>
-                    <input type="submit" value="Join/Create Room"/>
-                </form>
-            ) : roomData ? (
-                <div className="room-data">
-                    <div>Room Name: {roomData.room_name}</div>
-                    <div>Host: {roomData.host}</div>
-                    <div>Players: {roomData.players.map((player, index) => {
-                        return (
-                            <div key={player.username}>{player.connected ? player.username :
-                                <strike>{player.username}</strike>}</div>
-                        )
-                    })}</div>
-                </div>
-            ) : ''}
-            {/*{response ? <div>{response}</div> : <div>'No response'</div>}*/}
+        return (
+            <div className="login-screen">
+                <ConnectedIndicator connected={connected}/>
+                {!inRoom ? (
+                    <form onSubmit={handleSubmit} className={"join-room-form flex-col flex-center"}>
+                        <div className={inputClasses.room}><input type="text" value={roomName} onChange={handleChange}
+                                                                  placeholder="Room Name" name="roomName" required/></div>
+                        <div className={inputClasses.user}><input type="text" value={userName} onChange={handleChange}
+                                                                  placeholder="Nickname" name="userName" required/></div>
+                        <input type="submit" value="Join/Create Room"/>
+                    </form>
+                ) : roomData ? (
+                    <div className="room-data">
+                        <div>Room Name: {roomData.room_name}</div>
+                        <div>Host: {roomData.host}</div>
+                        <div>Players: {roomData.players.map((player, index) => {
+                            return (
+                                <div key={player.username}>{player.connected ? player.username :
+                                    <strike>{player.username}</strike>}</div>
+                            )
+                        })}</div>
+                        <button onClick={leaveRoom} className="btn w-max-content mx-auto">Leave Room</button>
+                    </div>
+                ) : ''}
+                {/*{response ? <div>{response}</div> : <div>'No response'</div>}*/}
 
-        </div>
-);
-}
+            </div>
+        );
+    }
 ;
 
 
--- stb_server.py
+++ stb_server.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 import socketio
-
+import hashlib
 
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 app = socketio.ASGIApp(sio)
 
-
 players = {}
-rooms = []
+rooms = {}
 
 
 class Player(BaseModel):
-    username: str
+    username: Optional[str] = None
     last_sid: str
+    last_client_ip: str
+    token: Optional[str] = None
     connected: bool = True
 
 
@@ -25,7 +26,23 @@ class Room(BaseModel):
 
 @sio.event
 async def connect(sid, environ, auth):
-    print(environ['asgi.scope']['client'])
+    player = Player(last_client_ip=environ['asgi.scope']['client'][0], last_sid=sid)
+    players[sid] = player
+    print(player.last_client_ip + ' connected!')
+    print(f'available rooms: {rooms}')
+
+
+@sio.event
+async def get_token(sid):
+    player = players.get(sid)
+    if player:
+        sha_sig = hashlib.sha256((player.last_client_ip).encode('utf-8')).hexdigest()
+        player.token = sha_sig
+        players[sha_sig] = player
+        print(f'token: {sha_sig}')
+        return sha_sig
+    else:
+        print('Player not found')
 
 
 @sio.event
@@ -33,16 +50,20 @@ async def connect_error(data):
     print("The connection failed!")
 
 
+async def get_player_by_sid(sid):
+    player = [player for player in players.values() if player.last_sid == sid][0]
+    return player
+
+
 @sio.event
 async def disconnect(sid):
-    for room in rooms:
+    player = await get_player_by_sid(sid)
+    for room in rooms.values():
         room_players = [player.username for player in room.players]
-        if players.get(sid) in room_players:
-            print(f'found {players.get(sid)}')
-            room.players[room_players.index(players[sid])].connected = False
+        if player.username in room_players:
+            player.connected = False
             await sio.emit('update room', room.dict())
-
-    print(f"{sid} disconnected!")
+    print(f"{player.last_client_ip} disconnected!")
 
 
 @sio.on('get response')
@@ -52,17 +73,42 @@ async def get_response(sid):
 
 
 @sio.event
-async def new_player(sid: str, room_name: str, username: str):
-    player = Player(username=username, last_sid=sid)
-    players[sid] = username
+async def new_player(sid: str, room_name: str, username: str, token: str):
+    player = players.get(token)
+    player = player if player is not None else Player(
+        last_sid=sid,
+        last_client_ip=None,
+    )
+    player.username = username
+    players[token] = player
     sio.enter_room(sid, room_name)
-    room_names = [room.room_name for room in rooms]
-    if room_name not in [room.room_name for room in rooms]:
-        room = Room(room_name=room_name, players=[player], host=username)
-        rooms.append(room)
+    if room_name not in rooms.keys():
+        room = Room(room_name=room_name, players=[players[token]], host=username)
+        rooms[room_name] = room
     else:
-        room = rooms[room_names.index(room_name)]
-        room.players.append(player)
-    if len(room.players) > 0:
-        await sio.emit('update room', room.dict())
+        room = rooms[room_name]
+        room_players = [player.username for player in room.players]
+        if username in room_players:
+            if not room.players[room_players.index(username)] == players[token]:
+                room.players[room_players.index(username)].connected = True
+                room.players[room_players.index(username)].last_sid = sid
+                await sio.emit('update room', room.dict())
+            else:
+                return {'message': 'username already taken'}
+        else:
+            room.players.append(players[token])
+            await sio.emit('update room', room.dict())
     return room.dict()
+
+
+@sio.event
+async def leave_room(sid: str, room_name: str, token: str):
+    sio.leave_room(sid, room_name)
+    for player in rooms[room_name].players:
+        if player.token == token:
+            print(f'removing {player.username}')
+            rooms[room_name].players.remove(player)
+            await sio.emit('update room', rooms[room_name].dict())
+            break
+    if len(rooms[room_name].players) == 0:
+        del rooms[room_name]
